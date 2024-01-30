b3913d38fe2836295f7f57e97a7ef2d84c4c4aa7
Gurpreet Singh <gurpreet.software.engg@gmail.com>
2024-01-30 13:42:36 +0530

    added Refresh button

--- src/App.js
+++ src/App.js
@@ -35,7 +35,10 @@ function App() {
   return (
     <>
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
-        <h1 className='text-white text-center my-3'>Password Generator</h1>
+        <div className='flex flex-row shadow rounded-lg overflow-hidden mb-4'>
+          <h1 className='text-white text-center my-3'>Password Generator</h1>
+          <button className='outline-none text-white px-3 py-0.5 float-right' onClick={passwordGenerator}>🔄</button>
+        </div>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
           <input type = "text" value = {password} className = "outline-none w-full py-1 px-3" placeholder='Password' readOnly ref = {passRef}/>
           <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'  onClick={copyToClipBoard}>Copy</button>
